42c5c2040524d4c293f4cde2cf299571fad9a944
JanNB97 <j.bingemann@gmx.de>
2018-12-03 20:54:23 +0100

    tests for multiple sudokus

--- src/test/java/janbingemann/SudokuGridTest.java
+++ src/test/java/janbingemann/SudokuGridTest.java
@@ -1,6 +1,9 @@
 package janbingemann;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
 
 public class SudokuGridTest
 {
@@ -9,42 +12,137 @@ public class SudokuGridTest
 	private static final int REGION_HEIGHT = 3;
 
 	@Test
-	public void testSudokuGrid()
+	public void testSudokuGrid1()
 	{
 		SudokuGrid sudokuGrid = new SudokuGrid(SIZE, REGION_WIDTH, REGION_HEIGHT);
 
-		sudokuGrid.setField(3, 3, 5);
-		System.out.println(sudokuGrid);
+		int y;
 
-		sudokuGrid.setField(0, 0, 1);
-		System.out.println(sudokuGrid);
+		y = 0;
+		this.setFieldSuccessfully(sudokuGrid, 1, y, 1);
+		this.setFieldSuccessfully(sudokuGrid, 2, y, 7);
+		this.setFieldSuccessfully(sudokuGrid, 6, y, 4);
 
-		System.out.println(sudokuGrid.setField(4, 4, 1));
-		System.out.println(sudokuGrid.setField(8, 8, 5));
-		System.out.println(sudokuGrid);
+		y = 1;
+		this.setFieldSuccessfully(sudokuGrid, 3, y, 2);
+		this.setFieldSuccessfully(sudokuGrid, 6, y, 8);
+		this.setFieldSuccessfully(sudokuGrid, 8, y, 6);
 
-		System.out.println(sudokuGrid.setField(5, 1, 5));
-		System.out.println(sudokuGrid);
+		y = 2;
+		this.setFieldSuccessfully(sudokuGrid, 3, y, 4);
+		this.setFieldSuccessfully(sudokuGrid, 5, y, 7);
+		this.setFieldSuccessfully(sudokuGrid, 8, y, 1);
 
-		System.out.println(sudokuGrid.setField(5, 1, 5));
-		System.out.println(sudokuGrid);
+		y = 3;
+		this.setFieldSuccessfully(sudokuGrid, 0, y, 3);
+		this.setFieldSuccessfully(sudokuGrid, 1, y, 6);
+		this.setFieldSuccessfully(sudokuGrid, 2, y, 5);
+		this.setFieldSuccessfully(sudokuGrid, 7, y, 4);
+		this.setFieldSuccessfully(sudokuGrid, 8, y, 9);
 
-		System.out.println(sudokuGrid.setField(0, 2, 5));
-		System.out.println(sudokuGrid);
+		y = 4;
+		this.setFieldSuccessfully(sudokuGrid, 0, y, 9);
+		this.setFieldSuccessfully(sudokuGrid, 2, y, 8);
+		this.setFieldSuccessfully(sudokuGrid, 4, y, 7);
 
-		System.out.println(sudokuGrid.setField(1, 7, 5));
-		System.out.println(sudokuGrid);
+		y = 5;
+		this.setFieldSuccessfully(sudokuGrid, 0, y, 7);
+		this.setFieldSuccessfully(sudokuGrid, 3, y, 3);
+		this.setFieldSuccessfully(sudokuGrid, 4, y, 5);
+		this.setFieldSuccessfully(sudokuGrid, 6, y, 6);
 
-		System.out.println(sudokuGrid.setField(2, 6, 5));
-		System.out.println(sudokuGrid);
+		y = 6;
+		this.setFieldSuccessfully(sudokuGrid, 0, y, 2);
+		this.setFieldSuccessfully(sudokuGrid, 1, y, 7);
+		this.setFieldSuccessfully(sudokuGrid, 4, y, 6);
+		this.setFieldSuccessfully(sudokuGrid, 5, y, 8);
+		this.setFieldSuccessfully(sudokuGrid, 6, y, 5);
 
-		System.out.println(sudokuGrid.setField(7, 0, 5));
-		System.out.println(sudokuGrid);
+		y = 7;
+		this.setFieldSuccessfully(sudokuGrid, 2, y, 6);
+		this.setFieldSuccessfully(sudokuGrid, 5, y, 3);
+		this.setFieldSuccessfully(sudokuGrid, 7, y, 9);
 
-		System.out.println(sudokuGrid.setField(2, 5, 5));
-		System.out.println(sudokuGrid);
+		y = 8;
+		this.setFieldSuccessfully(sudokuGrid, 0, y, 8);
+		this.setFieldSuccessfully(sudokuGrid, 1, y, 3);
+		this.setFieldSuccessfully(sudokuGrid, 2, y, 4);
+		this.setFieldSuccessfully(sudokuGrid, 5, y, 2);
 
-		System.out.println(sudokuGrid.setField(6, 4, 5));
-		System.out.println(sudokuGrid);
+		System.out.println(sudokuGrid.toString());
+	}
+
+	@Test
+	public void testSudokuGrid2()
+	{
+		SudokuGrid sudokuGrid = new SudokuGrid(SIZE, REGION_WIDTH, REGION_HEIGHT);
+
+		int y;
+
+		y = 0;
+		this.setFieldSuccessfully(sudokuGrid, 0, y, 4);
+		this.setFieldSuccessfully(sudokuGrid, 1, y, 1);
+		this.setFieldSuccessfully(sudokuGrid,4, y, 6);
+		this.setFieldSuccessfully(sudokuGrid,5, y, 5);
+		this.setFieldSuccessfully(sudokuGrid,8, y, 7);
+
+		y = 1;
+		this.setFieldSuccessfully(sudokuGrid, 2, y, 6);
+		this.setFieldSuccessfully(sudokuGrid, 5, y, 7);
+		this.setFieldSuccessfully(sudokuGrid, 6, y, 4);
+		this.setFieldSuccessfully(sudokuGrid, 7, y, 8);
+
+		y = 2;
+		this.setFieldSuccessfully(sudokuGrid, 0, y, 2);
+		this.setFieldSuccessfully(sudokuGrid, 2, y, 7);
+		this.setFieldSuccessfully(sudokuGrid, 3, y, 4);
+		this.setFieldSuccessfully(sudokuGrid, 4, y, 9);
+		this.setFieldSuccessfully(sudokuGrid, 8, y, 6);
+
+		y = 3;
+		this.setFieldSuccessfully(sudokuGrid, 1, y, 6);
+		this.setFieldSuccessfully(sudokuGrid, 4, y, 7);
+		this.setFieldSuccessfully(sudokuGrid, 6, y, 1);
+
+		y = 4;
+		this.setFieldSuccessfully(sudokuGrid, 0, y, 3);
+		this.setFieldSuccessfully(sudokuGrid, 2, y, 1);
+		this.setFieldSuccessfully(sudokuGrid, 3, y, 5);
+		this.setFieldSuccessfully(sudokuGrid, 7, y, 7);
+		this.setFieldSuccessfully(sudokuGrid, 8, y, 2);
+
+		y = 5;
+		this.setFieldSuccessfully(sudokuGrid, 1, y, 9);
+		this.setFieldSuccessfully(sudokuGrid, 4, y, 4);
+		this.setFieldSuccessfully(sudokuGrid, 5, y, 2);
+		this.setFieldSuccessfully(sudokuGrid, 6, y, 3);
+		this.setFieldSuccessfully(sudokuGrid, 8, y, 8);
+
+		y = 6;
+		this.setFieldSuccessfully(sudokuGrid, 0, y, 1);
+		this.setFieldSuccessfully(sudokuGrid, 2, y, 8);
+		this.setFieldSuccessfully(sudokuGrid, 3, y, 6);
+		this.setFieldSuccessfully(sudokuGrid, 7, y, 2);
+		this.setFieldSuccessfully(sudokuGrid, 8, y, 9);
+
+		y = 7;
+		this.setFieldSuccessfully(sudokuGrid, 1, y, 2);
+		this.setFieldSuccessfully(sudokuGrid, 4, y, 1);
+		this.setFieldSuccessfully(sudokuGrid, 5, y, 8);
+		this.setFieldSuccessfully(sudokuGrid, 6, y, 6);
+		this.setFieldSuccessfully(sudokuGrid, 7, y, 4);
+
+		y = 8;
+		this.setFieldSuccessfully(sudokuGrid, 0, y, 6);
+		this.setFieldSuccessfully(sudokuGrid, 3, y, 3);
+		//this.setFieldSuccessfully(sudokuGrid, 7, y, 1);
+
+		System.out.println(sudokuGrid.toString());
+	}
+
+	private void setFieldSuccessfully(SudokuGrid sudokuGrid, int x, int y, int digit)
+	{
+		Assert.assertThat(sudokuGrid.setField(x, y, digit), is(true));
+		Assert.assertThat(sudokuGrid.isUnsolvable(), is(false));
 	}
 }
